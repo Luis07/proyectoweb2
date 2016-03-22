fae0595c801ee3b3680639448fac18e816850a06
Luis07 <luisbrenes250594@gmail.com>
2016-03-22 11:18:00 -0600

    se agrego la función send_mail_confirm_registro
    se le envia un correo al usuario para que confirme su registro en
    nuestro sitio

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+use Mail;
+use Session;
+use Redirect; 
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class MailController extends Controller
+{
+	//para enviar el correo de confirmacion de registro al usuario
+ public function Send_mail_confirm_registro($para,$asunto,$contenido)
+ {
+ $desde="From:". "luisbrenes250594@gmail.com";
+ Mail($para,$asunto,$contenido,$desde);
+ echo "$para";
+ echo "$asunto";
+ echo "$contenido";
+    return view('login');	
+}
+}
